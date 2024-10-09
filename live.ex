caf08d0e3d1f71521f88e3968ffb1f90afd569f3
linqueta <lincolnrodrs@gmail.com>
2024-10-09 20:31:00 -0300

    feat: Improve menus

--- live.ex
+++ live.ex
@@ -21,8 +21,14 @@ defmodule Oban.Console.Queues do
     Enum.map(Oban.config().queues, fn {name, _} ->
       [queue: name]
       |> Oban.check_queue()
-      |> Map.take([:queue, :local_limit, :global_limit, :paused, :rate_limit])
+      |> Map.take([:queue, :paused, :local_limit])
     end)
+  end
+
+  def show_list() do
+    headers = [:queue, :paused, :local_limit]
+
+    Oban.Console.View.Table.show(list(), headers)
   end
 
   def pause(name), do: Oban.pause_queue(queue: name)
@@ -81,91 +87,187 @@ defmodule Oban.Console.Jobs do
   defp filter_by_limit(query, limit), do: limit(query, ^limit)
 end
 
-defmodule Oban.Console.Menus do
-  def print(:queues) do
-    """
-    Queues:
+defmodule Oban.Console.View.Printer do
+  @spec line(integer()) :: :ok
+  def line(size \\ 80), do: separator(size) |> IO.puts()
 
-    1. List
-    2. Pause
-    3. Resume
-    0. Return
-    """
+  @spec separator(integer()) :: String.t()
+  def separator(size \\ 80), do: String.pad_trailing("", size, "-")
+
+  def menu(label, items) do
+    IO.puts("")
+
+    profile = System.get_env("OBAN_CONSOLE_PROFILE")
+
+    if System.get_env("OBAN_CONSOLE_PROFILE") do
+      IO.write("[#{header_color(profile)}] ")
+    end
+
+    IO.puts(header_color(label))
+
+    Enum.each(items, fn {key, value} ->
+      IO.puts("#{header_color(to_string(key) <> ".")} #{value}")
+    end)
   end
 
-  def print(queues: :list) do
-    """
-    1. Pause
-    2. Resume
-    0. Return
-    """
+  @spec break() :: :ok
+  def break, do: IO.puts("")
+
+  def gets([header | remaining]) do
+    [header_color(header), remaining]
+    |> List.flatten()
+    |> Enum.join(" | ")
+    |> IO.gets()
+    |> String.trim()
   end
 
-  def print(:jobs) do
-    """
-    Jobs:
+  def header_color(text), do: IO.ANSI.light_blue() <> text <> IO.ANSI.reset()
 
-    1. List
-    2. Debug
-    3. Retry
-    4. Cancel
-    0. Return
-    """
+  def showable(value) when is_binary(value), do: value
+  def showable(value), do: inspect(value)
+
+  def red(text), do: IO.ANSI.red() <> text <> IO.ANSI.reset()
+  def green(text), do: IO.ANSI.green() <> text <> IO.ANSI.reset()
+end
+
+defmodule Oban.Console.View.Table do
+  alias Oban.Console.View.Printer
+
+  def show([record | _] = records, headers) when not is_map(record) do
+    records
+    |> Enum.map(&Map.from_struct/1)
+    |> show(headers)
   end
 
-  def print() do
-    """
-    Menus:
-    1. Queues
-    2. Jobs
-    0. Exit
-    """
+  def show([_ | _] = records, headers) do
+    rows =
+      records
+      |> Enum.map(fn r ->
+        Enum.map(headers, fn header -> Map.get(r, header) |> Printer.showable() end)
+      end)
+
+    headers = Enum.map(headers, &to_string/1)
+    table = [headers | rows]
+
+    indexes_pad = build_indexes_pad(table)
+
+    line_size = Enum.sum(indexes_pad) + 1 + length(indexes_pad) * 3
+
+    table_with_pads =
+      [build_row_with_pad(headers, indexes_pad, :header)]
+      |> append_list([Printer.separator(line_size)])
+      |> append_list(Enum.map(rows, fn row -> build_row_with_pad(row, indexes_pad, :row) end))
+      |> Enum.join("\n")
+
+    Printer.break()
+    Printer.line(line_size)
+
+    IO.puts(table_with_pads)
+    Printer.line(line_size)
+  end
+
+  defp append_list(list, value), do: list ++ value
+
+  defp build_row_with_pad(row, indexes_pad, type) do
+    formatted =
+      row
+      |> Enum.with_index()
+      |> Enum.map_join(" | ", fn {value, index} ->
+        value
+        |> String.pad_trailing(Enum.at(indexes_pad, index))
+        |> colorize(type)
+      end)
+
+    "| " <> formatted <> " |"
+  end
+
+  defp colorize(value, type), do: color_for(value, String.trim(value), type)
+
+  defp color_for(value, "false", :row), do: Printer.red(value)
+  defp color_for(value, "true", :row), do: Printer.green(value)
+  defp color_for(value, _, :header), do: Printer.header_color(value)
+  defp color_for(value, _, _), do: value
+
+  defp build_indexes_pad([headers | _] = table) do
+    headers
+    |> Enum.with_index()
+    |> Enum.map(fn {_, index} ->
+      Enum.reduce(table, 0, fn row, acc ->
+        value = Enum.at(row, index)
+        size = (value && String.length(value)) || 0
+
+        max(acc, size)
+      end)
+    end)
   end
 end
 
 defmodule Oban.Console.Interactive do
-  def start() do
-    IO.puts("")
-    IO.puts("Welcome to Oban Console")
+  alias Oban.Console.Queues
+  alias Oban.Console.View.Printer
 
+  def start() do
     initial_menu()
   end
 
   defp initial_menu() do
-    IO.puts(Oban.Console.Menus.print())
+    Printer.menu("Menu:", [{1, "Queues"}, {2, "Jobs"}, {3, "Profile"}, {0, "Exit"}])
 
-    case IO.gets("Select an option: ") do
-      "1\n" -> queues()
-      "2\n" -> jobs()
-      "0\n" -> IO.puts("Goodbye!")
+    case Printer.gets(["Select an option: "]) do
+      "1" -> queues()
+      "2" -> jobs()
+      "3" -> profile()
+      "0" -> Printer.menu("Goodbye!", [])
       _ -> initial_menu()
     end
   end
 
-  defp queues() do
-    IO.puts("")
-    IO.puts(Oban.Console.Menus.print(:queues))
+  defp profile() do
+    case Printer.gets(["Profile", "Select/Create your profile", "Name: "]) do
+      "0" -> initial_menu()
+      value -> System.put_env("OBAN_CONSOLE_PROFILE", value) && initial_menu()
+    end
+  end
 
-    case IO.gets("Select an option: ") do
-      "1\n" -> IO.puts("Option 1")
-      "2\n" -> IO.puts("Option 2")
-      "3\n" -> IO.puts("Option 3")
-      "0\n" -> initial_menu()
+  defp queues() do
+    Queues.show_list()
+    Printer.menu("Queues:", [{1, "List"}, {2, "Pause"}, {3, "Resume"}, {0, "Return"}])
+
+    case Printer.gets(["Select an option: "]) do
+      "1" -> queues()
+      "2" -> pause_queue()
+      "3" -> resume_queue()
+      "0" -> initial_menu()
       _ -> queues()
     end
   end
 
-  defp jobs() do
-    IO.puts("")
-    IO.puts(Oban.Console.Menus.print(:jobs))
+  defp resume_queue() do
+    case Printer.gets(["Resume", "Queue name: "]) do
+      "0" -> queues()
+      queue -> Queues.resume(queue) && Queues.show_list() && queues()
+    end
+  end
 
-    case IO.gets("Select an option: ") do
-      "1\n" -> IO.puts("Option 1")
-      "2\n" -> IO.puts("Option 2")
-      "3\n" -> IO.puts("Option 3")
-      "4\n" -> IO.puts("Option 4")
-      "0\n" -> initial_menu()
+  defp pause_queue() do
+    case Printer.gets(["Pause", "Queue name: "]) do
+      "0" -> queues()
+      queue -> Queues.pause(queue) && Queues.show_list() && queues()
+    end
+  end
+
+  defp jobs() do
+    Printer.menu("Jobs:", [{1, "List"}, {2, "Debug"}, {3, "Retry"}, {4, "Cancel"}, {0, "Return"}])
+
+    case Printer.gets(["Select an option: "]) do
+      "1" -> IO.puts("Option 1")
+      "2" -> IO.puts("Option 2")
+      "3" -> IO.puts("Option 3")
+      "4" -> IO.puts("Option 4")
+      "0" -> initial_menu()
       _ -> jobs()
     end
   end
 end
+
+Oban.Console.Interactive.start()
